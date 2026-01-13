d0befcde76704620d4907d656a5f10d85957e720
youness <younesshamou013@gmail.com>
2026-01-13 11:46:22 +0100

    Try pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Child } from './child/child';
 import { FormsModule } from '@angular/forms';
+import { UpperCasePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Child, FormsModule],
+  imports: [RouterOutlet, Child, FormsModule, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
