ea2eecc2e1a412a941074b1e9e5062965775d40c
youness <younesshamou013@gmail.com>
2026-01-13 09:32:29 +0100

    add form validation check

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,9 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('revision');
-  prix = signal(0);
-  quantite = signal(0);
-
-  total = computed(() => this.prix() * this.quantite());
+  isFormValid() : boolean{
+   return true
+  }
 }
