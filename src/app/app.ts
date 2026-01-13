3a7a6010dc79e3ef1abdcb64c0de49e854b530b2
youness <younesshamou013@gmail.com>
2026-01-13 12:12:53 +0100

    Pipe personnalisé

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,26 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, signal , PipeTransform, Pipe} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Child } from './child/child';
 import { FormsModule } from '@angular/forms';
 import { UpperCasePipe } from '@angular/common';
-
+ @Pipe({
+    name : "kebabCase"
+  })
+export  class KebabCasePipe implements PipeTransform{
+    transform(value:string):string{
+      return value.toLocaleLowerCase().replace(/ /g,'-');
+    }
+  }
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Child, FormsModule, UpperCasePipe],
+  imports: [RouterOutlet, Child, FormsModule, UpperCasePipe,KebabCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
+ 
+  
 export class App {
-  liste = signal<string[]>(["Angular","React","Laravel","Vue"])
-}
+liste = signal<string[]>(["Angular Framework", "React Library", "Laravel PHP", "Vue JS"]);
+  }
+
